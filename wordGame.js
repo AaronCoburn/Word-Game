936b3ccba862d67a44cfda96780e9feabf340bc5
fcc561aa587-4261-4d67-b9a9-16d10b9fbc52 <79658669+fcc561aa587-4261-4d67-b9a9-16d10b9fbc52@users.noreply.github.com>
2022-04-13 17:09:28 +0100

    Starting poitn

---
+++ wordGame.js
@@ -0,0 +1,22 @@
+/* This is me thinking about how I am going to make me a Word Game;
+What I want in my game;
+I want to have the diconary for the words to be created and stored inside the mapped blocks in the Level
+I want some of the words mapped to the blocks in the Level using arrays
+I want some of the words not to be mapped in the level for secret and extra pointing FileSystem
+I want the letter to appear in the bottom of the page and are able to be shuffled
+I always want there to be one or more words that are a total of the mac amount of letter in a Level
+I want the level to a certain amounts of hints 
+I want user to click and drag the word on to the empty spaced array at the missing word
+If user changes mind also want to be able to put the letter back on the hand in it's space or if stop missed the letter goes back home
+A hint will light up the word's letter empty to the hand's letter for the user to see where to place
+
+
+
+
+
+ I want the Levels to be Saveable for another time */
+
+ let wordDictionary = "The Word Dictionary";
+
+ 
+
